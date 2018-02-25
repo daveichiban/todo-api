2519879f55a6bd65eb16f1048d3236baac8dae08
Dave Shield <david.shield@gmail.com>
2018-02-25 16:59:13 +0000

    Added GET /todos route

--- server/server.js
+++ server/server.js
@@ -20,9 +20,16 @@ app.post("/todos", (req, res) => {
         res.status(400).send(e);
     }
 );
-
-
     console.log(req.body);
+});
+
+app.get("/todos", (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    })
+
 });
 
 
