9d620cb84f2d38d916e8f109e1a4d71edad44b6a
Dave Shield <david.shield@bcpsoftware.com>
2018-03-07 17:04:44 +0000

    Add token generation to POST /users route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require("./config/config");
 
+
 const _ = require("lodash");
 const { ObjectID } = require("mongodb");
 const express = require("express");
@@ -101,16 +102,17 @@ app.patch("/todos/:id", (req, res) => {
 
 
 app.post("/users", (req, res) => {
-    var user = new User (_.pick(req.body, ["email", "password"]))
-    user.save().then ((doc) => {
-        res.send(doc);
-    }, (e) => {
-        res.status(400).send(e);
-    }
-);
-    console.log(req.body);
-});
+    var user = new User (_.pick(req.body, ["email", "password"]));
 
+
+    user.save().then (() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header("x-auth", token).send(user);
+    }).catch ((e) => {
+        res.status(400).send(e);
+    })
+});
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
