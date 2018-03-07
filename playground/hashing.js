254601e486c04b9d1608fdc1722185acc289830a
Dave Shield <david.shield@bcpsoftware.com>
2018-03-07 17:04:11 +0000

    Playground

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,7 +1,20 @@
 const {SHA256} = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
-var message = "I am called Dave"
-var hash = SHA256(message).toString();
+var data = {
+    id: 10
+};
 
-console.log(message);
-console.log(hash);
+var token = jwt.sign(data,"123abc");
+
+console.log(token);
+
+var decode = jwt.verify(token, "123abc");
+
+console.log("Decoded: ", decode);
+
+// var message = "I am called Dave"
+// var hash = SHA256(message).toString();
+
+// console.log(message);
+// console.log(hash);
