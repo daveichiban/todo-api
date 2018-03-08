b515ab25d5fa897992f6ecadf9bddd3cfc97cf3c
Dave Shield <david.shield@bcpsoftware.com>
2018-03-08 15:47:08 +0000

    Add GET /users/me route

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const bodyParser = require("body-parser")
 const { mongoose } = require("./db/mongoose");
 const { Todo } = require("./models/todo");
 const { User } = require("./models/user");
+const { authenticate } = require("./middleware/authenticate")
 
 var app = express();
 
@@ -113,6 +114,12 @@ app.post("/users", (req, res) => {
         res.status(400).send(e);
     })
 });
+
+
+
+app.get("/users/me", authenticate, (req, res) => {
+    res.send(req.user);
+})
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
