1b0ab226338a04e34279898ccd653e4ac628b58a
Dave Shield <david.shield@bcpsoftware.com>
2018-06-11 14:17:32 +0100

    Added findByCredentials method

--- server/models/user.js
+++ server/models/user.js
@@ -55,6 +55,7 @@ UserSchema.methods.generateAuthToken = function () {
     });
 };
 
+
 UserSchema.statics.findByToken = function (token) {
     var User = this;
     var decoded;
@@ -70,6 +71,27 @@ UserSchema.statics.findByToken = function (token) {
         "tokens.access": "auth"
     })
 
+};
+
+UserSchema.statics.findByCredentials = function (email, password) {
+    var User = this;
+
+    return User.findOne({email}).then((user) => {
+        if(!user) {
+            return Promise.reject();
+        }
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if(res === true) {
+                    resolve(user);
+                } else {
+                    reject();
+                }
+            })
+        });
+
+
+    });
 };
 
 UserSchema.pre("save", function (next) {
